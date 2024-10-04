30e859a6206534d141baedb69545e7ed5c3efa5a
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-10-03 21:49:53 -0400

    deploying

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ function Contact(){
         <div className = 'contact'>
             <footer> 
             <p style = {{fontWeight: 'bold'}}> Let's Get In Touch! </p>
-            <a style = {{textDecoration: 'underline', color: 'black'}} href = "mailto:ahmedalamin957@gmail.com" target = '_blank' rel = "noopener norefferer"> Send An Email </a>
-            <p> Call & Text Me: 267-968-2802</p>
+            <p> ahmedalamin957@gmail.com </p>
+            <p> 267-968-2802</p>
 
             <div className="logos-container"> 
                 <a href="https://www.linkedin.com/in/ahmedalaminn" target="_blank" rel="noopener noreferrer"><img className="logos" src={linkedinLogo} alt="LinkedIn Logo" /></a>
